a6bf0240e01a4972df529752284645992d2686f2
John Prasad <blockmanbarry10@hotmail.com>
2025-02-21 19:50:19 +0000

    test changes for dark mode

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,11 +1,18 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
-    localStorage.setItem("darkMode", document.body.classList.contains("dark-mode") ? "enabled" : "disabled");
+
+    // Save dark mode preference in localStorage
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        localStorage.setItem("darkMode", "disabled");
+    }
 }
 
-// Check localStorage on page load
-window.onload = function() {
+// Apply dark mode settings on page load
+window.onload = function () {
     if (localStorage.getItem("darkMode") === "enabled") {
         document.body.classList.add("dark-mode");
     }
 };
+
